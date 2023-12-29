65e6d91736cc378d84c8481137af281dbc877381
MinwooJe <gpatration77@gmail.com>
2023-12-29 17:32:38 +0900

    Initial Commit

---
+++ HowsTheWear/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HowsTheWear
+//
+//  Created by 제민우 on 12/29/23.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
