05ef6088f374510366c66df05fb6b66ec794b1dd
Rafa <rafa71802937@gmail.com>
2024-01-08 15:05:33 +0900

    Remove: setupNavigationBarButtonSize 메서드 및 관련 코드 제거

--- HowsTheWear/Sources/WeatherScene/Views/WeatherBarButtonItem.swift
+++ HowsTheWear/Sources/WeatherScene/Views/WeatherBarButtonItem.swift
@@ -15,20 +15,5 @@ class WeatherBarButtonItem: UIView {
     let menuButton = UIButton().then {
         $0.setImage(UIImage(named: "menu"), for: .normal)
     }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupNavigationBarButtonSize()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupNavigationBarButtonSize() {
-        menuButton.snp.makeConstraints {
-            $0.width.height.equalTo(30)
-        }
-    }
 
 }
