5094d4dacefb00cdd877bdc669e0fe57839a4de6
MinwooJe <gpatration77@gmail.com>
2024-01-14 14:09:37 +0900

    fix: BrowseCollectionView CompositionalLayout 수정 - BrowseCollectionReusableView(헤더뷰) 구현

--- HowsTheWear/Sources/StyleScene/BrowseScene/Controllers/BrowseViewController.swift
+++ HowsTheWear/Sources/StyleScene/BrowseScene/Controllers/BrowseViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class BrowseViewController: UIViewController {
     
-    private var browseImageArray = [UIImage(named: "StyleTestImage"),
+    private var thisWeekStyleArray = [UIImage(named: "StyleTestImage"),
                                     UIImage(named: "StyleTestImage"),
                                     UIImage(named: "StyleTestImage"),
                                     UIImage(named: "StyleTestImage"),
                                     UIImage(named: "StyleTestImage"),
+                                    UIImage(named: "StyleTestImage"),
+                                    UIImage(named: "StyleTestImage"),
+                                    UIImage(named: "StyleTestImage")]
+    
+    private var nextWeekStyleArray = [UIImage(named: "StyleTestImage"),
+                                    UIImage(named: "StyleTestImage"),
+                                    UIImage(named: "StyleTestImage"),
+                                    UIImage(named: "StyleTestImage"),
+                                    UIImage(named: "StyleTestImage")]
+    
+    private var lastYearStyleArray = [UIImage(named: "StyleTestImage"),
                                     UIImage(named: "StyleTestImage"),
                                     UIImage(named: "StyleTestImage"),
                                     UIImage(named: "StyleTestImage")]
@@ -32,32 +43,36 @@ class BrowseViewController: UIViewController {
 // MARK: - Configure CollectionView
 
 extension BrowseViewController {
-    
+
     private func generateCollectionViewLayout() -> UICollectionViewCompositionalLayout {
         let itemSize = NSCollectionLayoutSize(
-            widthDimension: .absolute(105),
+            widthDimension: .fractionalWidth(1.0),
             heightDimension: .fractionalHeight(1.0)
         )
         
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 10)
         
         let groupSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(1.0),
-            heightDimension: .fractionalHeight(0.3)
+            widthDimension: .fractionalWidth(0.32),
+            heightDimension: .estimated(150)
         )
         
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        
-        let headerSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(1.0),
-            heightDimension: .fractionalHeight(0.15)
-        )
 
-        let headerElement = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        // screen의 height가 890 이상일 시 (XR, Plus, Max기종) header의 높이를 65로 설정 나머지는 35로 설정.
+        let screenHeight = UIScreen.main.bounds.height
+        let headerHeight: CGFloat = screenHeight >= 892 ? 65 : 35
+        
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(headerHeight))
+        let headerElement = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize,
+                                                                        elementKind: UICollectionView.elementKindSectionHeader,
+                                                                        alignment: .top)
         
         let section = NSCollectionLayoutSection(group: group)
-        section.boundarySupplementaryItems = [headerElement]
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0)
         section.orthogonalScrollingBehavior = .continuous
+        section.boundarySupplementaryItems = [headerElement]
 
         let layout = UICollectionViewCompositionalLayout(section: section)
         
@@ -68,12 +83,15 @@ extension BrowseViewController {
 // MARK: - Configure CollectionView
 
 extension BrowseViewController {
-    
+
     private func configureCollectionView() {
         browseCollectionView.dataSource = self
         browseCollectionView.register(BrowseCollectionViewCell.self, forCellWithReuseIdentifier: "browseCollectionViewCell")
         browseCollectionView.backgroundColor = .clear
         
+        browseCollectionView.register(BrowseCollectionReusableView.self,
+                                      forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                      withReuseIdentifier: BrowseCollectionReusableView.reuseIdentifier)
         // 모델, 데이터매니저 구현 후 데이터 받아오는 메서드 작성예정
     }
 }
@@ -87,20 +105,42 @@ extension BrowseViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return browseImageArray.count
+        switch section {
+        case 0:
+            return thisWeekStyleArray.count
+        case 1:
+            return nextWeekStyleArray.count
+        case 2:
+            return lastYearStyleArray.count
+        default:
+            return 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: "browseCollectionViewCell",
-            for: indexPath
-        ) as? BrowseCollectionViewCell else {
-            return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "browseCollectionViewCell", for: indexPath) as? BrowseCollectionViewCell else { return UICollectionViewCell() }
+        
+        let section = indexPath.section
+        
+        switch section {
+        case 0:
+            cell.styleImageView.image = thisWeekStyleArray[indexPath.item]
+        case 1:
+            cell.styleImageView.image = nextWeekStyleArray[indexPath.item]
+        case 2:
+            cell.styleImageView.image = lastYearStyleArray[indexPath.item]
+        default:
+            fatalError()
         }
-        
-        cell.styleImageView.image = browseImageArray[indexPath.item]
-        
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let headerView = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: BrowseCollectionReusableView.reuseIdentifier,
+            for: indexPath) as? BrowseCollectionReusableView else { fatalError("Cannot create new supplementary") }
+        return headerView
     }
 }
 
@@ -121,7 +161,7 @@ extension BrowseViewController {
             make.top.equalTo(safeArea.snp.top).offset(30)
             make.bottom.equalTo(safeArea.snp.bottom)
             make.leading.equalTo(safeArea.snp.leading).offset(15)
-            make.trailing.equalTo(safeArea.snp.trailing).offset(15)
+            make.trailing.equalTo(safeArea.snp.trailing)
         }
     }
 }
---
+++ HowsTheWear/Sources/StyleScene/BrowseScene/Views/BrowseCollectionReusableView.swift
@@ -0,0 +1,58 @@
+//
+//  BrowseCollectionReusableView.swift
+//  HowsTheWear
+//
+//  Created by 제민우 on 1/13/24.
+//
+
+import UIKit
+
+// BrowseCollectionView Header View
+final class BrowseCollectionReusableView: UICollectionReusableView {
+    
+    static let reuseIdentifier = "browseCollectionViewHeader"
+    
+    let browseHeaderLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 15, weight: .semibold)
+        $0.textColor = .black
+        $0.textAlignment = .left
+        $0.text = "헤더 레이블"
+    }
+    
+    private let browseHeaderRightArrowButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        $0.imageView?.contentMode = .scaleAspectFit
+        $0.tintColor = .black
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureSubviews()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Configure UI
+extension BrowseCollectionReusableView {
+    private func configureSubviews() {
+        [browseHeaderLabel, browseHeaderRightArrowButton].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func configureLayout() {
+        browseHeaderLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.bottom.equalToSuperview()
+        }
+        
+        browseHeaderRightArrowButton.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-16)
+        }
+    }
+}
