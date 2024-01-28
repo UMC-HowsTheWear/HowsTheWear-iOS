c4727d8b95704d740846fd826bdddf8298cc1f08
MinwooJe <gpatration77@gmail.com>
2024-01-28 17:46:57 +0900

    feat: PagingImageCollectionViewCell 구현

--- HowsTheWear/Sources/StyleScene/BrowseDetailScene/Views/PagingImageCollectionViewCell.swift
+++ HowsTheWear/Sources/StyleScene/BrowseDetailScene/Views/PagingImageCollectionViewCell.swift
@@ -9,4 +9,36 @@ import UIKit
 
 final class PagingImageCollectionViewCell: UICollectionViewCell {
     
+    let pagingImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureSubview()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+// MARK: Configure UI
+extension PagingImageCollectionViewCell {
+    private func configureSubview() {
+        contentView.addSubview(pagingImageView)
+    }
+    
+    private func configureLayout() {
+        let safeArea = contentView.safeAreaLayoutGuide
+        
+        pagingImageView.snp.makeConstraints { make in
+            make.margins.equalToSuperview()
+        }
+    }
+    
 }
