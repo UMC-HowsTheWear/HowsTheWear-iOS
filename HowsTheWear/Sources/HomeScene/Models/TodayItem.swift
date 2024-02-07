893c45c3cf82b7554003ca13ff613980c38743cb
rafa-e1 <rafa71802937@gmail.com>
2024-02-07 11:44:37 +0900

    add: 오늘의 아이템 더미 데이터 생성

---
+++ HowsTheWear/Sources/HomeScene/Models/TodayItem.swift
@@ -0,0 +1,21 @@
+//
+//  TodayItem.swift
+//  HowsTheWear
+//
+//  Created by RAFA on 2/7/24.
+//
+
+import Foundation
+
+struct TodayItem {
+    let image: String
+    let name: String
+}
+
+extension TodayItem {
+    static let items: [TodayItem] = [
+        TodayItem(image: "1", name: "니트"),
+        TodayItem(image: "2", name: "아우터"),
+        TodayItem(image: "3", name: "머플러")
+    ]
+}
