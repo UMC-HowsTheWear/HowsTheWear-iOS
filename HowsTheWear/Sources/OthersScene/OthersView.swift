a931bdd4c7a8f72443933e50f389078b0b7a29ee
rafa-e1 <rafa71802937@gmail.com>
2024-02-07 11:46:26 +0900

    add: 다른 사람들 폴더 및 파일 생성

---
+++ HowsTheWear/Sources/OthersScene/OthersView.swift
@@ -0,0 +1,20 @@
+//
+//  OthersView.swift
+//  HowsTheWear
+//
+//  Created by RAFA on 2/7/24.
+//
+
+import UIKit
+
+class OthersView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ HowsTheWear/Sources/OthersScene/OthersViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OthersViewController.swift
+//  HowsTheWear
+//
+//  Created by RAFA on 2/7/24.
+//
+
+import UIKit
+
+class OthersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
