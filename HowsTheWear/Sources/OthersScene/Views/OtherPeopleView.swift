f01f562bfb42c16d169026b70282e00bb0771bc7
MinwooJe <gpatration77@gmail.com>
2024-02-07 23:10:04 +0900

    feat: OthersPeopleView 구현

--- HowsTheWear/Sources/OthersScene/Views/OtherPeopleView.swift
+++ HowsTheWear/Sources/OthersScene/Views/OtherPeopleView.swift
@@ -7,14 +7,150 @@
 
 import UIKit
 
-class OtherPeopleView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class OtherPeopleView: UIView {
+    
+    private var hashTagNumber: Int = 5
+    
+    private var hashTagTitleArray: [String] = ["#캐쥬얼", "#스트릿", "#페미닌", "#미니멀"]
+  
+    private var thisWeekStyleArray:[UIImage?] = [
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage")
+    ]
+    
+    private var nextWeekStyleArray:[UIImage?] = [
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage")
+    ]
+    
+    private var lastYearStyleArray:[UIImage?] = [
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage"),
+        UIImage(named: "StyleTestImage")
+    ]
+    
+    private let othersPeopleCollectionView = BrowseMainCollectionView()
+    
+    let myPostButton = UIButton().then {
+        $0.backgroundColor = .black
+        $0.setImage(UIImage(systemName: "plus")?
+            .withConfiguration(
+                UIImage.SymbolConfiguration(pointSize: 20, weight: .regular)), for: .normal)
+        $0.tintColor = .white
     }
-    */
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configureInitialSetting()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        fetchData()
+        configureInitialSetting()
+        configureSubview()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
+
+// MARK: - Configure InitialSetting
+
+extension OtherPeopleView {
+    private func configureInitialSetting() {
+        myPostButton.layer.cornerRadius = myPostButton.frame.height / 2
+        myPostButton.layer.shadowColor = UIColor.black.cgColor
+        myPostButton.layer.shadowOpacity = 0.5
+        myPostButton.layer.shadowOffset = CGSize(width: 0, height: 0)
+        myPostButton.layer.shadowRadius = 10
+    }
+    
+}
+
+// MARK: - SetUp Data
+
+extension OtherPeopleView {
+    private func fetchData() {
+        othersPeopleCollectionView.configureContents(
+            sectionCount: hashTagNumber,
+            imagesData: [thisWeekStyleArray, nextWeekStyleArray, lastYearStyleArray, thisWeekStyleArray, nextWeekStyleArray],
+            sectionTitles: hashTagTitleArray
+        )
+    }
+}
+
+// MARK: - Configure UI
+
+extension OtherPeopleView {
+    private func configureSubview() {
+        [othersPeopleCollectionView, myPostButton].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func configureLayout() {
+        othersPeopleCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).inset(30)
+            make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom)
+            make.leading.equalTo(safeAreaLayoutGuide.snp.leading).inset(20)
+            make.trailing.equalTo(safeAreaLayoutGuide.snp.trailing)
+        }
+        
+        myPostButton.snp.makeConstraints {
+            $0.right.equalTo(-15)
+            $0.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).inset(15)
+            $0.width.height.equalTo(55)
+        }
+    }
+    
+}
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+struct UIViewPreview<View: UIView>: UIViewRepresentable {
+    let view: View
+
+    init(_ builder: @escaping () -> View) {
+        view = builder()
+    }
+
+    // MARK: - UIViewRepresentable
+
+    func makeUIView(context: Context) -> UIView {
+        return view
+    }
+
+    func updateUIView(_ view: UIView, context: Context) {
+        view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+    }
+}
+
+#endif
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct Preview: PreviewProvider{
+    static var previews: some View {
+        UIViewPreview {
+            let myView = OtherPeopleView()
+            return myView
+        }.previewLayout(.sizeThatFits)
+    }
+}
+#endif
