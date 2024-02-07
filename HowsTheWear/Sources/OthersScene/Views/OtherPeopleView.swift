e2228d4e60cd1e07f609d87b5af462b70263eba7
MinwooJe <gpatration77@gmail.com>
2024-02-07 21:21:03 +0900

    feat: OtherPeople뷰 선언

---
+++ HowsTheWear/Sources/OthersScene/Views/OtherPeopleView.swift
@@ -0,0 +1,20 @@
+//
+//  OtherPeopleView.swift
+//  HowsTheWear
+//
+//  Created by 제민우 on 2/7/24.
+//
+
+import UIKit
+
+class OtherPeopleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
