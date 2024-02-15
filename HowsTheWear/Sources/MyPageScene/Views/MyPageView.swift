8349cfc7cf35b89c4e354b6626c5ae686647b1fd
MinwooJe <gpatration77@gmail.com>
2024-02-15 14:18:10 +0900

    feat: MyPageView 선언

---
+++ HowsTheWear/Sources/MyPageScene/Views/MyPageView.swift
@@ -0,0 +1,20 @@
+//
+//  MyPageView.swift
+//  HowsTheWear
+//
+//  Created by 제민우 on 2/15/24.
+//
+
+import UIKit
+
+final class MyPageView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
