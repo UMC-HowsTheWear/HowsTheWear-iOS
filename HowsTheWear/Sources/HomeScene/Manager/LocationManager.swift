c6eee4adf0c846ec361e3ae00b45cbed75e972ee
rafa-e1 <rafa71802937@gmail.com>
2024-02-19 13:57:41 +0900

    feat: 사용자 위치 API 구현

--- HowsTheWear/Sources/HomeScene/Manager/LocationManager.swift
+++ HowsTheWear/Sources/HomeScene/Manager/LocationManager.swift
@@ -7,40 +7,77 @@
 
 import CoreLocation
 
-final class LocationManager: NSObject, CLLocationManagerDelegate {
-
-    private let manager = CLLocationManager()
-
+class LocationManager: NSObject {
+    
     static let shared = LocationManager()
-
+    
+    private let locationManager = CLLocationManager()
     private var locationFetchCompletion: ((CLLocation) -> Void)?
-
-    var location: CLLocation? {
+    
+    var locationStatus: CLAuthorizationStatus?
+    var onLocationChange: ((CLLocation?) -> Void)?
+    var onAuthStatusChange: ((CLAuthorizationStatus?) -> Void)?
+    
+    private var lastLocation: CLLocation? {
         didSet {
-            guard let location else {
+            guard let lastLocation else {
                 return
             }
-            locationFetchCompletion?(location)
+            locationFetchCompletion?(lastLocation)
         }
     }
 
-    public func getCurrentLocation(completion: @escaping (CLLocation) -> Void) {
+    private var statusString: String {
+        guard let status = locationStatus else {
+            return "unknown"
+        }
+        
+        switch status {
+        case .notDetermined: return "notDetermined"
+        case .authorizedWhenInUse: return "authorizedWhenInUse"
+        case .authorizedAlways: return "authorizedAlways"
+        case .restricted: return "restricted"
+        case .denied: return "denied"
+        default: return "unknown"
+        }
+    }
+    
+    override init() {
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+    }
 
+    func getCurrentLocation(completion: @escaping (CLLocation) -> Void) {
         self.locationFetchCompletion = completion
 
-        manager.requestWhenInUseAuthorization()
-        manager.delegate = self
-        manager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
     }
 
-    // MARK: - Location
+    func startLocationManagerIfNeeded() {
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+    
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        self.locationStatus = status
+        print(#function, statusString)
+        onAuthStatusChange?(status)
+    }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.first else {
-            return
-        }
-        self.location = location
-        manager.stopUpdatingLocation()
+        guard let location = locations.last else { return }
+        self.lastLocation = location
+        print(#function, location)
+        onLocationChange?(location)
+        locationManager.stopUpdatingLocation()
     }
     
 }
