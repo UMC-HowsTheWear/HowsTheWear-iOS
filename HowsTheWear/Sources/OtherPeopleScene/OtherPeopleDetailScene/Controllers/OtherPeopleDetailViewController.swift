0fd3eac3988b2f6bdf175096a60c41e93846c5fb
MinwooJe <gpatration77@gmail.com>
2024-02-21 07:05:37 +0900

    feat: OtherPeopleDetailView itemTagLabel 구현

--- HowsTheWear/Sources/OtherPeopleScene/OtherPeopleDetailScene/Controllers/OtherPeopleDetailViewController.swift
+++ HowsTheWear/Sources/OtherPeopleScene/OtherPeopleDetailScene/Controllers/OtherPeopleDetailViewController.swift
@@ -92,7 +92,26 @@ extension OtherPeopleDetailViewController {
     }
     
     @objc private func bookmarkButtonDidTapped() {
-        print("HI")
+        
+        let transparentColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+        let semiTransparentColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.66)
+        let textColor = UIColor(red: 0.878, green: 0.878, blue: 0.878, alpha: 1)
+        
+        UIView.animate(withDuration: 0.2) {
+            if self.otherPeopleDetailView.itemTagLabelContainerView.backgroundColor?.isEqual(transparentColor) ?? false {
+                self.otherPeopleDetailView.itemTagLabelContainerView.backgroundColor = semiTransparentColor
+                let transition = CATransition()
+                transition.duration = 0.3
+                self.otherPeopleDetailView.itemTagLabel.layer.add(transition, forKey: nil)
+                self.otherPeopleDetailView.itemTagLabel.textColor = textColor
+            } else {
+                self.otherPeopleDetailView.itemTagLabelContainerView.backgroundColor = transparentColor
+                let transition = CATransition()
+                transition.duration = 0.3
+                self.otherPeopleDetailView.itemTagLabel.layer.add(transition, forKey: nil)
+                self.otherPeopleDetailView.itemTagLabel.textColor = transparentColor
+            }
+        }
     }
     
     @objc private func myPostButtonDidTap() {
